d59667ae7b72efa819fc9e86fc0da379d55097c6
Panupong <vipa111190@gmail.com>
2025-10-03 12:13:58 +0700

    Update test05.c

---
+++ test05.c
@@ -0,0 +1,18 @@
+// โปรแกรมแสดงชื่อ Sombat และคะแนน 99 ทางหน้าจอ
+#include <stdio.h>
+
+int main()
+{
+
+    char fillname[10] = "Sombat";
+    int score;
+
+    score = 99;
+
+    // ใน " " เขียน printf() เขียนอย่างไรแสดงอย่างนั้น ยกเว้น % format spec., \ escape sequence.
+    printf("Name is : ", fullname\n);
+    printf("Name is : %s\n", fullname);
+    printf("Score is : %d\n", score);
+
+    return 0;
+}
