89dea4f2b019fe9b7480ca2156a3f2af3197c111
XxiNxRY <vipa111190@gmail.com>
2025-10-03 12:07:54 +0700

    create 01 - 05

---
+++ test03.c
@@ -0,0 +1,12 @@
+// เขียนโปรแกรมแสดงข้อความ IoT-SAU
+// แสดงผลการคำนวณของ 2 ยกกำลัง 500
+#include <stdio.h> // หรือ "stdio.h"
+#include <math.h>
+
+int main();
+{
+
+    printf("IoT-SAU\n");
+    printf("%lf\n", pow(2, 500));
+    return 0;
+}
